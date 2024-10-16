2abff33f371c7a639348bb43920fb855bccbc394
skiparb <skiparb@gmail.com>
2024-10-16 08:44:23 +0200

    kodzik 16.10.24

--- Account/urls.py
+++ Account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from Account import views
 
-urlpatterns = {
+urlpatterns = [
     path('login/', views.user_login, name = "login"),
-}
+]
--- Account/views.py
+++ Account/views.py
@@ -1,3 +1,7 @@
+from http.client import HTTPResponse
+
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from Account.forms import LoginForm
@@ -5,7 +9,18 @@ from Account.forms import LoginForm
 
 def user_login(request):
     if request.method == 'POST':
-        pass
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Uwierzytelnianie zakończyło się sukcesem")
+                else:
+                    return HttpResponse("Konto jest zablokowane")
+        else:
+            return HttpResponse("Nieprawidłowe dane")
     else:
         form = LoginForm()
     return render(request, template_name='account/login.html',context={'form':form})
